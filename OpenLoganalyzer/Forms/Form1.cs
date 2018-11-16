c55bca99dd507a4f7373aa0eb725ee9cd3e6c9c0
XanatosX <simonaberle@gmx.de>
2018-11-16 21:55:33 +0100

    Restructured tests Starting to add new test for configuration loader

--- OpenLoganalyzer/Forms/Form1.cs
+++ OpenLoganalyzer/Forms/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OpenLoganalyzer.Core.Enum;
+using OpenLoganalyzer.Core.Factories;
+using OpenLoganalyzer.Core.Interfaces;
+using OpenLoganalyzer.Core.LoaderCofiguration;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +23,30 @@ namespace OpenLoganalyzer
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ILoaderConfigurationLoader configLoader = new SimpleConfigurationLoader();
+            
+
+            Dictionary<string, string> filters = new Dictionary<string, string>();
+            filters.Add(FilterTypeEnum.Caller.ToString(), "^[A-Z][a-zA-z\\/]* ");
+            filters.Add(FilterTypeEnum.Datetime.ToString(), "-> [0-9]{2}:[0-9]{2}:[0-9]{2}:");
+            filters.Add(FilterTypeEnum.Message.ToString(), " [a-zA-Z][a-zA-Z:\\\\/ !0-9-]*");
+            filters.Add(FilterTypeEnum.Severity.ToString(), "INFO|DEBUG|WARNING|ERROR|CRITICAL");
+
+            Dictionary<string, string> additionalSettings = new Dictionary<string, string>();
+            additionalSettings.Add(AdditionalSettingsEnum.FilePath.ToString(), @"F:\Onedrive\Work share\output.log");
+            additionalSettings.Add(AdditionalSettingsEnum.DateTimeFormat.ToString(), "'->' HH':'mm':'ss':'");
+
+
+            ILoaderConfiguration configuration = new SimpleConfiguration(Core.Enum.LoaderTypeEnum.FileLoader, filters, additionalSettings);
+
+            ILoaderFactory loader = new LoaderFactory();
+            ILoader realLoder = loader.GetLoader(configuration);
+
+
+            List<ILogLine> lines = realLoder.Load();
+
+            configLoader.Save(configuration, @"F:\Onedrive\Work share\config.json");
+
         }
     }
 }
---
+++ OpenLoganalyzerTests/Tests/SimpleConfigurationLoaderTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OpenLoganalyzer.Core.Interfaces;
+using OpenLoganalyzer.Core.LoaderCofiguration;
+using OpenLoganalyzerTests.Helper;
+
+namespace OpenLoganalyzerTests
+{
+    [TestClass]
+    public class SimpleConfigurationLoaderTest
+    {
+        private string testFile;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            testFile = testFile.CreateTestPath();
+        }
+
+        [TestMethod]
+        public void TestBasicSetupUp()
+        {
+            SimpleConfigurationLoader simpleConfigurationLoader = new SimpleConfigurationLoader();
+        
+            Assert.IsTrue(simpleConfigurationLoader != null);
+        }
+
+        [TestMethod]
+        public void TestBasicSaving()
+        {
+            SimpleConfigurationLoader simpleConfigurationLoader = new SimpleConfigurationLoader();
+
+            Mock<ILoaderConfiguration> configuration = new Mock<ILoaderConfiguration>();
+            configuration.Setup(x => x.LoaderType).Returns(OpenLoganalyzer.Core.Enum.LoaderTypeEnum.FileLoader);
+            configuration.Setup(x => x.Filters).Returns(new System.Collections.Generic.Dictionary<string, string>());
+            configuration.Setup(x => x.FilterNames).Returns(new System.Collections.Generic.List<string>());
+
+            simpleConfigurationLoader.Save(configuration.Object, testFile);
+
+            Assert.IsTrue(simpleConfigurationLoader != null);
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            File.Delete(testFile);
+        }
+    }
+}
