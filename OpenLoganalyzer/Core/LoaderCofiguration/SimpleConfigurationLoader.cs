1e28f7bc278c9ce3a94e2ee43acfba08ad5f1c9c
XanatosX <simonaberle@gmx.de>
2018-11-16 16:25:14 +0100

    Added basic loader for configurations

---
+++ OpenLoganalyzer/Core/LoaderCofiguration/SimpleConfigurationLoader.cs
@@ -0,0 +1,46 @@
+﻿using OpenLoganalyzer.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace OpenLoganalyzer.Core.LoaderCofiguration
+{
+    public class SimpleConfigurationLoader : ILoaderConfigurationLoader
+    {
+        public ILoaderConfiguration Load(string pathToFile)
+        {
+            ILoaderConfiguration returnConfiguration = null;
+
+            if (!File.Exists(pathToFile))
+            {
+                return returnConfiguration;
+            }
+
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            try
+            {
+                string content = File.ReadAllText(pathToFile);
+                returnConfiguration = JsonConvert.DeserializeObject<SimpleConfiguration>(content);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return returnConfiguration;
+        }
+
+        public bool Save(ILoaderConfiguration configuration, string filePath)
+        {
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                jsonSerializer.Serialize(writer, configuration);
+            }
+            return true;
+        }
+    }
+}
