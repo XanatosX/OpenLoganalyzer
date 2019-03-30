fca27ae54e965f7fe3323f1dc86de0c2d93616b8
XanatosX <simonaberle@gmx.de>
2019-03-30 12:07:59 +0100

    Update ILoaderFactory.cs
    Added DocBlock

--- OpenLoganalyzerLib/Core/Interfaces/ILoaderFactory.cs
+++ OpenLoganalyzerLib/Core/Interfaces/ILoaderFactory.cs
@@ -8,6 +8,11 @@ namespace OpenLoganalyzerLib.Core.Interfaces
 {
     public interface ILoaderFactory
     {
+        /// <summary>
+        /// This function will get you the correct loader for the configuration provided
+        /// </summary>
+        /// <param name="configuration">The configuration to use</param>
+        /// <returns>This will return you an ILoader to load your configuration</returns>
         ILoader GetLoader(ILoaderConfiguration configuration);
     }
 }
