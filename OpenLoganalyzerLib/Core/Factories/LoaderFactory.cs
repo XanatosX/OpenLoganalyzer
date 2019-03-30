12b6de50c3c577d7bfb38d61030edeea73e4da0d
XanatosX <simonaberle@gmx.de>
2019-03-30 12:08:12 +0100

    Update LoaderFactory.cs
    Added missing summary block

--- OpenLoganalyzerLib/Core/Factories/LoaderFactory.cs
+++ OpenLoganalyzerLib/Core/Factories/LoaderFactory.cs
@@ -11,6 +11,11 @@ namespace OpenLoganalyzerLib.Core.Factories
 {
     public class LoaderFactory : ILoaderFactory
     {
+        /// <summary>
+        /// This function will get you the correct loader for the configuration provided
+        /// </summary>
+        /// <param name="configuration">The configuration to use</param>
+        /// <returns>This will return you an ILoader to load your configuration</returns>
         public ILoader GetLoader(ILoaderConfiguration configuration)
         {
             ILoader returnValue = null;
