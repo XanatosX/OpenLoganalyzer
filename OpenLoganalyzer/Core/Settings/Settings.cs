65721eebe2cf0c20cd939aae6bab042237a0f3d1
XanatosX <simonaberle@gmx.de>
2019-03-31 18:45:05 +0200

    Create Settings.cs
    Added simple setting class

---
+++ OpenLoganalyzer/Core/Settings/Settings.cs
@@ -0,0 +1,35 @@
+﻿using OpenLoganalyzer.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLoganalyzer.Core.Settings
+{
+    public class Settings : SettingsInterface
+    {
+        private readonly Dictionary<string, string> settings;
+        Dictionary<string, string> SettingsInterface.Settings => settings;
+
+        public Settings()
+        {
+            settings = new Dictionary<string, string>();
+        }
+
+        public void AddSetting(string settingName, string settingValue)
+        {
+            settings.Add(settingName, settingValue);
+        }
+
+        public string GetSetting(string settingsName)
+        {
+            string returnValue = String.Empty;
+            if (settings.ContainsValue(settingsName)) {
+                returnValue = settings[settingsName];
+            }
+
+           return returnValue;
+        }
+    }
+}
