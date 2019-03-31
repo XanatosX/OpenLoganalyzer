c3e7eb88f1760a744e95ac01dbd855a3bc466e15
XanatosX <simonaberle@gmx.de>
2019-03-31 13:12:00 +0200

    Update JsonSimpleConfiguration.cs
    Added missing summary blocks

--- OpenLoganalyzerLib/Core/LoaderCofiguration/JsonSimpleConfiguration.cs
+++ OpenLoganalyzerLib/Core/LoaderCofiguration/JsonSimpleConfiguration.cs
@@ -10,15 +10,27 @@ namespace OpenLoganalyzerLib.Core.LoaderCofiguration
 {
     public class JsonSimpleConfiguration : IJsonLoaderConfiguration
     {
+        /// <summary>
+        /// The type of the loader to use
+        /// </summary>
         private readonly LoaderTypeEnum loaderType;
         public LoaderTypeEnum LoaderType => loaderType;
 
+        /// <summary>
+        /// All the filters for the logfile to check
+        /// </summary>
         private readonly Dictionary<string, string> filters;
         public Dictionary<string, string> Filters => filters;
 
+        /// <summary>
+        /// All the additional settings for the container
+        /// </summary>
         private readonly Dictionary<string, string> additionalSettingContainer;
         public Dictionary<string, string> AdditionalSettingContainer => additionalSettingContainer;
 
+        /// <summary>
+        /// Empty constructur for the Json config class
+        /// </summary>
         public JsonSimpleConfiguration()
         {
             loaderType = LoaderTypeEnum.FileLoader;
@@ -26,6 +38,12 @@ namespace OpenLoganalyzerLib.Core.LoaderCofiguration
             additionalSettingContainer = new Dictionary<string, string>();
         }
 
+        /// <summary>
+        /// Constructor to parse the simple configuration to the json format
+        /// </summary>
+        /// <param name="loaderTypeEnum">The loader type to use for loading</param>
+        /// <param name="newFilters">The fitlers to save</param>
+        /// <param name="additionalSettings">The settings to save</param>
         public JsonSimpleConfiguration(LoaderTypeEnum loaderTypeEnum,
             Dictionary<string, string> newFilters,
             Dictionary<string, string> additionalSettings)
@@ -35,6 +53,10 @@ namespace OpenLoganalyzerLib.Core.LoaderCofiguration
             additionalSettingContainer = additionalSettings;
         }
 
+        /// <summary>
+        /// Convert this class to a configuration class
+        /// </summary>
+        /// <returns>SimpleConfiguration class</returns>
         public ILoaderConfiguration GetLoaderConfiguration()
         {
             return new SimpleConfiguration(loaderType, filters, additionalSettingContainer);
