8afb6681912efe46ce1b99c839242f4e54387368
XanatosX <simonaberle@gmx.de>
2019-03-31 19:27:48 +0200

    Create WindowExtension.cs
    Added style method to extension methods

---
+++ OpenLoganalyzer/Core/Extensions/WindowExtension.cs
@@ -0,0 +1,28 @@
+﻿using OpenLoganalyzer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenLoganalyzer.Core.Extensions
+{
+    public static class WindowExtension
+    {
+        public static bool ChangeStyle(this Window currentWindow, string themeName)
+        {
+            currentWindow.Resources.MergedDictionaries.Clear();
+            try
+            {
+                Uri uri = new Uri("/OpenLoganalyzer;component/Styles/" + themeName + ".xaml", UriKind.Relative);
+                currentWindow.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = uri });
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
