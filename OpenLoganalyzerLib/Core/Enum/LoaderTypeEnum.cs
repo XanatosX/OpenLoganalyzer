8962d8b16d7ba9947a5e11689531f804f19a41e9
XanatosX <simonaberle@gmx.de>
2019-03-31 13:02:14 +0200

    Update LoaderTypeEnum.cs
    Added summary block

--- OpenLoganalyzerLib/Core/Enum/LoaderTypeEnum.cs
+++ OpenLoganalyzerLib/Core/Enum/LoaderTypeEnum.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace OpenLoganalyzerLib.Core.Enum
 {
+    /// <summary>
+    /// Enum of all the available loader types for the factory
+    /// </summary>
     public enum LoaderTypeEnum
     {
         FileLoader
