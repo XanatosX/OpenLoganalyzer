d04bf3062f088136f8bb763a150af46f2132f2ed
XanatosX <simonaberle@gmx.de>
2019-04-01 20:06:36 +0200

    Added style manager classes
    Added style container manager and enum class

---
+++ OpenLoganalyzer/Core/Style/StyleDict.cs
@@ -0,0 +1,47 @@
+﻿using OpenLoganalyzer.Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenLoganalyzer.Core.Style
+{
+    public class StyleDict
+    {
+        private readonly StyleEnum styleType;
+        public StyleEnum StyleType => styleType;
+
+        private readonly string path;
+        public string Path => path;
+
+        private readonly string name;
+        public string Name => name;
+
+        public StyleDict(StyleEnum type, string filePath)
+        {
+            styleType = type;
+            path = filePath;
+            string fileName = filePath.Split('/').Last();
+            name = fileName.Split('.').First();
+        }
+
+        public ResourceDictionary GetDictionary()
+        {
+            ResourceDictionary dict = null;
+            switch (styleType)
+            {
+                case StyleEnum.buildin:
+                    Uri uri = new Uri(path, UriKind.Relative);
+                    dict = new ResourceDictionary() { Source = uri };
+                    break;
+                case StyleEnum.file:
+                    break;
+                default:
+                    break;
+            }
+            return dict;
+        }
+    }
+}
---
+++ OpenLoganalyzer/Core/Style/StyleManager.cs
@@ -0,0 +1,44 @@
+﻿using OpenLoganalyzer.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenLoganalyzer.Core.Style
+{
+    public class StyleManager
+    {
+        private readonly List<StyleDict> styles;
+        public List<StyleDict> Styles => styles;
+
+        public StyleManager()
+        {
+            styles = new List<StyleDict>();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("StyleNames.txt"));
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while (reader.Peek() >0)
+                    {
+                        string currentLine = reader.ReadLine();
+                        StyleDict style = new StyleDict(Enum.StyleEnum.buildin, currentLine);
+                        styles.Add(style);
+                    }
+                    
+                }
+            }
+        }
+
+        public void ScanFolder(string FolderPath)
+        {
+
+        }
+    }
+}
