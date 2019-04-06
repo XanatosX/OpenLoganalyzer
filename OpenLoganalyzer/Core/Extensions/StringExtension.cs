13928bf9ca56d6bbad5e2323afbc333327ccb45f
XanatosX <simonaberle@gmx.de>
2019-04-06 11:47:24 +0200

    Create StringExtension.cs
    Added new string extension class

---
+++ OpenLoganalyzer/Core/Extensions/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenLoganalyzer.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static string GetTranslatedString(this string target, string key)
+        {
+            target = Application.Current.FindResource("key") as string;
+            if (target == null)
+            {
+                target = string.Empty;
+            }
+
+            return target;
+        }
+    }
+}
