dc1ceb26cc5b5139b629e190c4c574691a9337e6
XanatosX <simonaberle@gmx.de>
2019-10-19 01:42:41 +0200

    Moved class
    Adding new method to read files in folder

--- OpenLoganalyzer/Core/Filter/FilterManager.cs
+++ OpenLoganalyzer/Core/Filter/FilterManager.cs
@@ -5,28 +5,48 @@ using OpenLoganalyzerLib.Core.Interfaces.Loader;
 using OpenLoganalyzerLib.Core.LoaderCofiguration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenLoganalyzer.Core
+namespace OpenLoganalyzer.Core.Filter
 {
     public class FilterManager
     {
         private readonly List<string> filterFiles;
         public List<string> Filters => filterFiles;
 
+        private readonly string folder;
+
         private readonly ISettings settings;
 
         private readonly ILoaderConfigurationLoader loaderConfigurationLoader;
 
-        public FilterManager(ISettings settingsManager)
+        public FilterManager(ISettings settings, string filterFolder)
         {
+            folder = filterFolder;
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
             filterFiles = new List<string>();
-            settings = settingsManager;
+            this.settings = settings;
             ILoaderConfigurationLoader loaderConfigurationLoader = new SimpleConfigurationLoader();
             loaderConfigurationLoader.LoadingError += LoaderConfigurationLoader_LoadingError;
-            
+        }
+
+        public List<FileInfo> getAvailableFilters()
+        {
+            List<FileInfo> returnFilters = new List<FileInfo>();
+             
+            foreach (string fileName in Directory.GetFiles(folder))
+            {
+                
+                returnFilters.Add(new FileInfo(fileName));
+            }
+
+            return returnFilters;
         }
 
         private void LoaderConfigurationLoader_LoadingError(object sender, EventArgs e)
