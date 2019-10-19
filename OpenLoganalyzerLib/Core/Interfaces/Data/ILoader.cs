d8624ba5e5baed39e8edf98ec71bfc594928fd88
XanatosX <simonaberle@gmx.de>
2019-10-19 12:46:44 +0200

    Reworked ILoader interface

--- OpenLoganalyzerLib/Core/Interfaces/Data/ILoader.cs
+++ OpenLoganalyzerLib/Core/Interfaces/Data/ILoader.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenLoganalyzerLib.Core.Interfaces.Loader
+namespace OpenLoganalyzerLib.Core.Interfaces.Data
 {
     public interface ILoader
     {
