84a7098951051e89ba33bc40368ac15718365b8b
XanatosX <simonaberle@gmx.de>
2019-10-20 00:52:02 +0200

    Adding interface

---
+++ OpenLoganalyzerLib/Core/Interfaces/Configuration/IFilterColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLoganalyzerLib.Core.Interfaces.Configuration
+{
+    public interface IFilterColumn
+    {
+        string Type { get; }
+
+        List<string> PossibleRegex { get; }
+
+        void addNewRegex(string regex);
+
+        void addMultipleRegex(List<string> regexList);
+
+        void addMultipleRegex(string[] regexList);
+
+        bool IsValid(string logLine);
+
+        string GetMatchingPart(string logLine);
+    }
+}
