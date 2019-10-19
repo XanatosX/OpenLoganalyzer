39a1e055c71967ffa54a5cee729a17eff1530eab
XanatosX <simonaberle@gmx.de>
2019-10-19 23:21:34 +0200

    Adding new interfaces and class instances

---
+++ OpenLoganalyzerLib/Core/Interfaces/LogAnalyzing/ILogLine.cs
@@ -0,0 +1,16 @@
+﻿using OpenLoganalyzerLib.Core.Interfaces.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLoganalyzerLib.Core.Interfaces.LogAnalyzing
+{
+    public interface ILogLine
+    {
+        Dictionary<ILogLineFilter, string> FilteredLogLine { get; }
+
+        void Init(string logLine, IFilter filter);
+    }
+}
---
+++ OpenLoganalyzerLib/Core/Interfaces/LogAnalyzing/ILogManager.cs
@@ -0,0 +1,18 @@
+﻿using OpenLoganalyzerLib.Core.Interfaces.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLoganalyzerLib.Core.Interfaces.LogAnalyzing
+{
+    public interface ILogManager
+    {
+        void Init(IFilter filterToUse);
+
+        ILogLine GetLogLine(string logLine);
+
+        Enumerable<ILogLine> GetLogLines(Enumerable<string> logLines);
+    }
+}
