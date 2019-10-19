4b464ac6096a5d7c3ea6b30622785ea1d5cf8aad
XanatosX <simonaberle@gmx.de>
2019-10-19 12:56:20 +0200

    Adding first parts for this class

--- OpenLoganalyzerLib/Core/Configuration/Loader/JsonFilterLoader.cs
+++ OpenLoganalyzerLib/Core/Configuration/Loader/JsonFilterLoader.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using OpenLoganalyzerLib.Core.Interfaces.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OpenLoganalyzerLib.Core.Configuration.Loader
 {
-    public class JsonFilterLoader
+    public class JsonFilterLoader : IFilterLoader
     {
+        private readonly string folderPath;
+
+        public JsonFilterLoader(string folderPath)
+        {
+            this.folderPath = folderPath;
+            if (!this.folderPath.EndsWith("\\"))
+            {
+                this.folderPath += "\\";
+            }
+        }
+
+        public IFilter LoadFilterByName(string name)
+        {
+            string fileName = folderPath + name + ".json";
+            if (!File.Exists(fileName))
+            {
+                return default;
+            }
+
+            //@TODO: Write the logic to load the filters again after saving them!!!
+            return default;
+        }
     }
 }
+
