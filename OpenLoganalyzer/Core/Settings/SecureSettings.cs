1f55fd7b933f66bda606fa81816470c7e54ec81d
XanatosX <simonaberle@gmx.de>
2019-10-25 09:21:09 +0200

    Adding new class to secure strings

---
+++ OpenLoganalyzer/Core/Settings/SecureSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLoganalyzer.Core.Settings
+{
+    public class SecureSettings
+    {
+    }
+}
